cc67bf612bea380829a9832551ef45cd95767ce3
Will Ross <paxswill@gmail.com>
2014-03-19 13:27:30 -0400

    Start creating division admin pages
    Can list and create divisions, and see the members of divisions.

--- evesrp/views.py
+++ evesrp/views.py
@@ -1,11 +1,18 @@
-from . import app, auth_methods
-
 from flask import render_template, redirect, url_for, request
 from flask.ext.login import login_user, login_required, logout_user
 from flask.ext.wtf import Form
 from wtforms.fields import StringField, PasswordField, SelectField, SubmitField
 from wtforms.widgets import HiddenInput
 from wtforms.validators import InputRequired
+
+from . import app, auth_methods, db
+
+from .auth.models import Division
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('base.html')
 
 
 class SelectValueField(SelectField):
@@ -21,19 +28,12 @@ class LoginForm(Form):
             coerce=int)
 
 
-@app.route('/')
-@login_required
-def index():
-    return render_template('base.html')
-
-
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if len(auth_methods) < 2:
         form.auth_method.widget = HiddenInput()
     if form.validate_on_submit():
-        print('Form validated')
         method = auth_methods[form.auth_method.data]
         user = method.authenticate_user(form.username.data, form.password.data)
         if user is not None:
@@ -47,3 +47,32 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/divisions')
+@login_required
+def list_divisions():
+    return render_template('divisions.html', divisions=Division.query.all())
+
+
+class AddDivisionForm(Form):
+    name = StringField('Division Name', validators=[InputRequired()])
+
+
+@app.route('/division/add', methods=['GET', 'POST'])
+@login_required
+def add_division():
+    form = AddDivisionForm()
+    if form.validate_on_submit():
+        division = Division(form.name.data)
+        db.session.add(division)
+        db.session.commit()
+        return redirect(url_for('division_detail', division_id=division.id))
+    return render_template('form.html', form=form)
+
+
+@app.route('/division/<division_id>')
+@login_required
+def division_detail(division_id):
+    division = Division.query.get_or_404(division_id)
+    return render_template('division_detail.html', division=division)
