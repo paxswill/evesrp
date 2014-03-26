f217e795cc1f741af23ea5c6356dbdcd97a57534
Will Ross <paxswill@gmail.com>
2014-03-26 10:49:15 -0400

    Require a user agent for remote requests to be set

--- evesrp/__init__.py
+++ evesrp/__init__.py
@@ -9,8 +9,6 @@ from flask.ext.login import LoginManager
 from flask.ext.principal import Principal
 
 requests_session = requests.Session()
-requests_session.headers.update(
-        {'User-Agent': 'EVE-SRP/0.1 (paxswill@paxswill.com)'})
 
 app = Flask('evesrp')
 # SQLALCHEMY_DATABASE_URI gets set by the Heroku extension frmo the
@@ -25,7 +23,6 @@ locale.setlocale(locale.LC_ALL, '')
 
 # Auth setup
 auth_methods = []
-
 @app.before_first_request
 def _copy_config_to_authmethods():
     for method in app.config['AUTH_METHODS']:
@@ -33,6 +30,23 @@ def _copy_config_to_authmethods():
         module = importlib.import_module(module_name)
         method_class = getattr(module, class_name)
         auth_methods.append(method_class(config=app.config))
+
+
+# Requests session setup
+user_agent = ''
+@app.before_first_request
+def _config_requests_session():
+    try:
+        ua_string = app.config['USER_AGENT_STRING']
+    except KeyError as outer_exc:
+        try:
+            ua_string = 'EVE-SRP/0.1 ({})'.format(
+                    app.config['USER_AGENT_EMAIL'])
+        except KeyError as inner_exc:
+            raise inner_exc from outer_exc
+    requests_session.headers.update({'User-Agent': ua_string})
+    user_agent = ua_string
+
 
 # Views setup
 from . import views
--- run.py
+++ run.py
@@ -4,4 +4,5 @@ from evesrp import app
 import config
 
 app.config.from_object(config.Development)
+app.config['USER_AGENT_EMAIL'] = 'paxswill@paxswill.com'
 app.run()
