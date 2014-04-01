64fae51970df74d676c93a4b9afe3b23ec7aa210
Will Ross <paxswill@gmail.com>
2014-04-01 18:29:48 -0400

    Only start the app when run as a script

--- run.py
+++ run.py
@@ -13,4 +13,6 @@ class SQLZKillmail(ZKillmail, SQLShipNameMixin): pass
 app.config.from_object(config.Development)
 app.config['USER_AGENT_EMAIL'] = 'paxswill@paxswill.com'
 app.config['KILLMAIL_SOURCES'] = [CRESTMail, SQLZKillmail]
-app.run()
+
+if __name__ == '__main__':
+    app.run()
