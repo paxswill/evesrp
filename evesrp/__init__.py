b53db1921742eb42edd69f199a7f6ceb2048e91e
Will Ross <paxswill@gmail.com>
2014-04-02 13:46:59 -0400

    Move request focused view functions into a module

--- evesrp/__init__.py
+++ evesrp/__init__.py
@@ -55,6 +55,6 @@ def _config_killmails():
 
 # Views setup
 from . import views
-from .views import divisions, login
+from .views import login, divisions, requests
 
 login_manager.login_view = 'login'
