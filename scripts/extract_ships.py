b0045e02fe5f0ab80e2fe6158cb3674c336afb78
Will Ross <paxswill@gmail.com>
2014-04-23 17:02:14 -0400

    Fix ship extracting script to get all ships
    Not all ships can be sold on the market, so you should go through invCategories to get them.

--- scripts/extract_ships.py
+++ scripts/extract_ships.py
@@ -14,77 +14,42 @@ engine = create_engine(argv[1])
 metadata = MetaData(bind=engine)
 
 
+invCategories = Table('invCategories', metadata, autoload=True)
+invGroups = Table('invGroups', metadata, autoload=True)
 invTypes = Table('invTypes', metadata, autoload=True)
-
-
-invMarketGroups = Table('invMarketGroups', metadata, autoload=True)
-
-
-def get_group_types(group_id, conn):
-    sel = select([invTypes.c.typeID])
-    sel = sel.where(invTypes.c.marketGroupID == group_id)
-    result = conn.execute(sel)
-    type_ids = set(map(lambda x: x[0], result))
-    result.close()
-    return type_ids
-
-
-def walk_groups(group_id, conn):
-    type_ids = set()
-    # if there're items in here, get the first
-    sel = select([invMarketGroups.c.hasTypes])
-    sel = sel.where(invMarketGroups.c.marketGroupID == group_id)
-    sel = sel.limit(1)
-    result = conn.execute(sel)
-    row = result.fetchone()
-    result.close()
-    if row[0] == 1:
-        sel = select([invTypes.c.typeID])
-        sel = sel.where(invTypes.c.marketGroupID == group_id)
-        result = conn.execute(sel)
-        type_ids.update(map(lambda x: x[0], result))
-        result.close()
-    # get subgroups
-    sel = select([invMarketGroups.c.marketGroupID])
-    sel = sel.where(invMarketGroups.c.parentGroupID == group_id)
-    # Recurse into subgroups to find their IDs
-    result = conn.execute(sel)
-    for row in result:
-        type_ids.update(walk_groups(row[0], conn))
-    result.close()
-    return type_ids
-
-
-def find_ship_ids(conn):
-    # Construct query to find the root ship market group
-    sel = select([invMarketGroups.c.marketGroupID])
-    sel = sel.where(invMarketGroups.c.marketGroupName == 'Ships')
-    sel = sel.where(invMarketGroups.c.parentGroupID == None)
-    sel = sel.limit(1)
-    result = conn.execute(sel)
-    row = result.fetchone()
-    result.close()
-    # And now we recurse
-    return walk_groups(row[0], conn)
-
-
-def map_names(conn, type_ids):
-    ship_names = {}
-    base_sel = select([invTypes.c.typeName])
-    for type_id in type_ids:
-        sel = base_sel.where(invTypes.c.typeID == type_id).limit(1)
-        result = conn.execute(sel)
-        row = result.fetchone()
-        ship_names[type_id] = row[0]
-        result.close()
-    return ship_names
 
 
 if __name__ == '__main__':
     conn = engine.connect()
-    ship_ids = find_ship_ids(conn)
-    names = map_names(conn, ship_ids)
+
+    # Get the Ship category ID
+    cat_sel = select([invCategories.c.categoryID]).\
+            where(invCategories.c.categoryName == 'Ship').limit(1)
+    result = conn.execute(cat_sel)
+    cat_id = result.fetchone()[0]
+    result.close()
+
+    # Get groups in this category
+    group_sel = select([invGroups.c.groupID]).where(
+            invGroups.c.categoryID == cat_id)
+    group_result = conn.execute(group_sel)
+
+    # get typeIDs and names
+    names = {}
+    for group_row in group_result:
+        group_id = group_row[0]
+        type_sel = select([invTypes.c.typeID, invTypes.c.typeName]).\
+                where(invTypes.c.groupID == group_id)
+        type_result = conn.execute(type_sel)
+        for row in type_result:
+            names[row[0]] = row[1]
+        type_result.close()
+
+    # Cleanup
+    group_result.close()
     conn.close()
+
+    # Write the file
     try:
         output_name = argv[2]
     except IndexError:
