bd0aefd0c07097aced908179b8331b25b9d6f2d1
Will Ross <paxswill@gmail.com>
2014-04-23 16:19:42 -0400

    Update heroku.py for new killmail sources

--- heroku.py
+++ heroku.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 from evesrp import create_app
-from evesrp.killmail import CRESTMail, ShipURLMixin
+from evesrp.killmail import CRESTMail, ZKillmail, ShipURLMixin
 from evesrp.auth.testauth import TestAuth
 from os import environ as env
 from binascii import unhexlify
 
 
-skel_url = 'https://wiki.eveonline.com/en/wiki/{name}'
+eve_wiki_url = 'https://wiki.eveonline.com/en/wiki/{name}'
+EveWikiMixin = ShipURLMixin(eve_wiki_url)
 
-class EOWikiCREST(CRESTMail, ShipURLMixin(skel_url)): pass
+class EveWikiCRESTMail(CRESTMail, EveWikiMixin): pass
+
+class EveWikiZKillmail(ZKillmail, EveWikiMixin): pass
+
 
 
 app = create_app()
@@ -19,7 +23,11 @@ app.config['AUTH_METHODS'] = [TestAuth()]
 app.config['CORE_AUTH_PRIVATE_KEY'] = env.get('CORE_PRIVATE_KEY')
 app.config['CORE_AUTH_PUBLIC_KEY'] = env.get('CORE_PUBLIC_KEY')
 app.config['CORE_AUTH_IDENTIFIER'] = env.get('CORE_IDENTIFIER')
-app.config['KILLMAIL_SOURCES'] = [EOWikiCREST]
+app.config['KILLMAIL_SOURCES'] = [
+        EveWikiZKillmail,
+        EveWikiCRESTMail
+]
+
 if env.get('DEBUG') is not None:
     app.debug = True
 
