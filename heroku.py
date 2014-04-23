dcc0d0373b6cb72a62c3279ae753f7cf0663f63b
Will Ross <paxswill@gmail.com>
2014-04-23 11:55:50 -0400

    Use new AuthMethod configuration for Heroku

--- heroku.py
+++ heroku.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from evesrp import create_app
 from evesrp.killmail import CRESTMail, ShipURLMixin
-import evesrp.auth.testauth
+from evesrp.auth.testauth import TestAuth
 from os import environ as env
 from binascii import unhexlify
 
@@ -15,7 +15,7 @@ app = create_app()
 app.config['SQLALCHEMY_DATABASE_URI'] = env['DATABASE_URL']
 app.config['SECRET_KEY'] = unhexlify(env['SECRET_KEY'])
 app.config['USER_AGENT_EMAIL'] = 'paxswill@paxswill.com'
-app.config['AUTH_METHODS'] = ['evesrp.auth.testauth.TestAuth']
+app.config['AUTH_METHODS'] = [TestAuth()]
 app.config['CORE_AUTH_PRIVATE_KEY'] = env.get('CORE_PRIVATE_KEY')
 app.config['CORE_AUTH_PUBLIC_KEY'] = env.get('CORE_PUBLIC_KEY')
 app.config['CORE_AUTH_IDENTIFIER'] = env.get('CORE_IDENTIFIER')
@@ -25,5 +25,7 @@ if env.get('DEBUG') is not None:
 
 
 if __name__ == '__main__':
+    # So we get the database tables for these
+    from evesrp.auth.testauth import TestUser, TestGroup
     print("Creating databases...")
     app.extensions['sqlalchemy'].db.create_all(app=app)
