f21a2af84e656ba0fa9170c0dfdf21189f4a66eb
Will Ross <paxswill@gmail.com>
2014-05-04 11:15:33 -0400

    Wrap the typeahead JS so it's only run on the division pages

--- evesrp/static/js/custom-typeahead.js
+++ evesrp/static/js/custom-typeahead.js
@@ -2,59 +2,61 @@
  * Autocomplete for divisions detail
  */
 
-var users = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.nonword('name'),
-  queryTokenizer: Bloodhound.tokenizers.nonword,
-  prefetch: {
-    url: $SCRIPT_ROOT + '/api/user/',
-    filter: function(list) {
-      return list['users'];
-    }
+if ($('.entity-typeahead').length) {
+  /* seth the entity ID for the appropriate input box */
+  function setEntityID(ev, datum, dataset) {
+    $(this).closest('form').find("input[name='id_']").attr('value', datum['id']);
   }
-});
-
-var groups = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.nonword('name'),
-  queryTokenizer: Bloodhound.tokenizers.nonword,
-  prefetch: {
-    url: $SCRIPT_ROOT + '/api/group/',
-    filter: function(list) {
-      return list['groups'];
+  /* Setup the data sources */
+  var users = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.nonword('name'),
+    queryTokenizer: Bloodhound.tokenizers.nonword,
+    prefetch: {
+      url: $SCRIPT_ROOT + '/api/user/',
+      filter: function(list) {
+        return list['users'];
+      }
     }
-  }
-});
-
-groups.initialize();
-users.initialize();
-
-$('.entity-typeahead').typeahead({
-  hint: true,
-  highlight: true
-},
-{
-  name: 'users',
-  displayKey: 'name',
-  source: users.ttAdapter(),
-  templates: {
-    suggestion: function(obj) {
-      return '<p>' + obj['name'] + ' <small class="text-muted">User</small></p>';
+  });
+  var groups = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.obj.nonword('name'),
+    queryTokenizer: Bloodhound.tokenizers.nonword,
+    prefetch: {
+      url: $SCRIPT_ROOT + '/api/group/',
+      filter: function(list) {
+        return list['groups'];
+      }
     }
-  }
-},
-{
-  name: 'groups',
-  displayKey: 'name',
-  source: groups.ttAdapter(),
-  templates: {
-    suggestion: function(obj) {
-      return '<p>' + obj['name'] + ' <small class="text-muted">Group</small></p>';
-    }
-  }
-})
-.on('typeahead:autocompleted', setEntityID)
-.on('typeahead:selected', setEntityID)
-.on('typeahead:cursorchanged', setEntityID);
+  });
+  groups.initialize();
+  users.initialize();
 
-function setEntityID(ev, datum, dataset) {
-  $(this).closest('form').find("input[name='id_']").attr('value', datum['id']);
+  /* Create the typeahead */
+  $('.entity-typeahead').typeahead({
+    hint: true,
+    highlight: true
+  },
+  {
+    name: 'users',
+    displayKey: 'name',
+    source: users.ttAdapter(),
+    templates: {
+      suggestion: function(obj) {
+        return '<p>' + obj['name'] + ' <small class="text-muted">User</small></p>';
+      }
+    }
+  },
+  {
+    name: 'groups',
+    displayKey: 'name',
+    source: groups.ttAdapter(),
+    templates: {
+      suggestion: function(obj) {
+        return '<p>' + obj['name'] + ' <small class="text-muted">Group</small></p>';
+      }
+    }
+  })
+  .on('typeahead:autocompleted', setEntityID)
+  .on('typeahead:selected', setEntityID)
+  .on('typeahead:cursorchanged', setEntityID);
 }
