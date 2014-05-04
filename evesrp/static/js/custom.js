ddb34a3e3e075c392248ce8ffd9b8ca39e4beb52
Will Ross <paxswill@gmail.com>
2014-05-04 12:49:07 -0400

    Make event listeners for request dropdowns more unique

--- evesrp/static/js/custom.js
+++ evesrp/static/js/custom.js
@@ -2,14 +2,14 @@
  * Make the link in button dropdowns submit the form
  */
 
-$("ul#action-type li a").click( function (e) {
+$("ul#request-action-type li a").click( function (e) {
   var form = $(this).closest("form");
   form.find("input[name='type_']").attr("value", $(this).attr("id"));
   form.submit();
   return false;
 });
 
-$("ul#modifier-type li a").click( function(e) {
+$("ul#request-modifier-type li a").click( function(e) {
   var form = $(this).closest("form");
   form.find("input[name='type_']").attr("value", $(this).attr("id"));
   form.submit();
