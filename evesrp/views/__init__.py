3d1678e4b9194eacbf6cd08f5c9c65d33970c1ef
Will Ross <paxswill@gmail.com>
2014-05-08 10:12:24 -0400

    The root url now redirects to a user's personal list

--- evesrp/views/__init__.py
+++ evesrp/views/__init__.py
@@ -1,8 +1,8 @@
-from flask import render_template
+from flask import redirect, url_for
 from flask.ext.login import login_required
 
 
 @login_required
 def index():
     """The index page for EVE-SRP."""
-    return render_template('base.html')
+    return redirect(url_for('requests.personal_requests'))
