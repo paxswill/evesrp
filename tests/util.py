8cca853dd3d0fbc0b5242dd1299d243f106e8b56
Will Ross <paxswill@gmail.com>
2014-05-22 00:09:04 -0400

    Refactor TestLogin to make logging in more reusable
    TestLogin is renamed to TestLoginView and TestLogin is now a utility class.

--- tests/util.py
+++ tests/util.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 from evesrp import create_app, db
+from evesrp.auth import AuthMethod, AuthForm, User
+from wtforms.fields import StringField
+from sqlalchemy.orm.exc import NoResultFound
+from flask import redirect, url_for, request, render_template
 
 
 class TestApp(TestCase):
@@ -11,3 +15,73 @@ class TestApp(TestCase):
         self.app.config['USER_AGENT_EMAIL'] = 'testing@example.com'
         self.app.config['WTF_CSRF_ENABLED'] = False
         db.create_all(app=self.app)
+
+
+class NullAuthForm(AuthForm):
+    name = StringField()
+
+
+class NullAuth(AuthMethod):
+    """A bare-bones AuthMethod that does not do any password checking."""
+
+    def login(self, form):
+        """Log in a previously registered user."""
+        try:
+            user = User.query.filter_by(name=form.name.data,
+                    authmethod=self.name).one()
+        except NoResultFound:
+            return redirect(url_for('login.login'))
+        self.login_user(user)
+        return redirect(request.args.get('next') or url_for('index'))
+
+    def view(self):
+        """Register a User."""
+        form = NullAuthForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(name=form.name.data,
+                    authmethod=self.name).first()
+            if user is None:
+                user = User(form.name.data, self.name)
+                db.session.add(user)
+                db.session.commit()
+                self.login_user(user)
+                return redirect(url_for('index'))
+            else:
+                abort(400)
+        return render_template('form.html', form=form)
+
+    def form(self):
+        return NullAuthForm
+
+
+class TestLogin(TestApp):
+
+    def setUp(self):
+        super(TestLogin, self).setUp()
+        self.auth_methods = [
+                NullAuth(name='Null Auth 1'),
+                NullAuth(name='Null Auth 2'),
+        ]
+        self.app.config['AUTH_METHODS'] = self.auth_methods
+        self.normal_name = 'Normal User'
+        self.admin_name = 'Admin User'
+        with self.app.test_request_context():
+            db.session.add(User(self.normal_name, self.auth_methods[0].name))
+            admin_user = User(self.admin_name, self.auth_methods[0].name)
+            admin_user.admin = True
+            db.session.add(admin_user)
+            db.session.commit()
+
+    def login(self, username=None):
+        if username is None:
+            username = self.normal_name
+        client = self.app.test_client()
+        data = {
+            'name': username,
+            'submit': 'true',
+        }
+        auth_method = self.auth_methods[0]
+        data = {auth_method.safe_name + '-' + field: value for field, value in
+                data.items()}
+        client.post('/login/', follow_redirects=True, data=data)
+        return client
--- tests/views/test_login.py
+++ tests/views/test_login.py
@@ -1,58 +1,10 @@
-from ..util import TestApp
+from ..util import TestApp, TestLogin
 from evesrp import db
 from evesrp.auth import AuthMethod, AuthForm
 from evesrp.auth.models import User
-from wtforms.fields import StringField
-from sqlalchemy.orm.exc import NoResultFound
-from flask import redirect, url_for, request, render_template
 
 
-class NullAuthForm(AuthForm):
-    name = StringField()
-
-
-class NullAuth(AuthMethod):
-    """A bare-bones AuthMethod that does not do any password checking."""
-
-    def login(self, form):
-        """Log in a previously registered user."""
-        try:
-            user = User.query.filter_by(name=form.name.data,
-                    authmethod=self.name).one()
-        except NoResultFound:
-            return redirect(url_for('login.login'))
-        self.login_user(user)
-        return redirect(request.args.get('next') or url_for('index'))
-
-    def view(self):
-        """Register a User."""
-        form = NullAuthForm()
-        if form.validate_on_submit():
-            user = User.query.filter_by(name=form.name.data,
-                    authmethod=self.name).first()
-            if user is None:
-                user = User(form.name.data, self.name)
-                db.session.add(user)
-                db.session.commit()
-                self.login_user(user)
-                return redirect(url_for('index'))
-        return render_template('form.html', form=form)
-
-    def form(self):
-        return NullAuthForm
-
-
-class TestNullAuth(TestApp):
-
-    def setUp(self):
-        super(TestNullAuth, self).setUp()
-        self.auth_methods = [
-                NullAuth(name='Null Auth 1'),
-                NullAuth(name='Null Auth 2'),
-        ]
-        self.app.config['AUTH_METHODS'] = self.auth_methods
-
-class TestTrampolineView(TestNullAuth):
+class TestTrampolineView(TestLogin):
 
     def test_account_creation(self):
         """Tests that the AuthMethod-specific views are being rendered
@@ -61,7 +13,7 @@ class TestTrampolineView(TestNullAuth):
         with self.app.test_client() as c:
             # Actual request
             resp = c.post('/login/null_auth_1/', follow_redirects=True, data={
-                'name': 'Normal User',
+                'name': 'Unique User',
                 'submit': 'true',
             })
             self.assertIn(b'Log Out', resp.data)
@@ -69,18 +21,18 @@ class TestTrampolineView(TestNullAuth):
                     .first())
 
 
-class TestLogin(TestNullAuth):
+class TestLoginView(TestLogin):
 
     def setUp(self):
-        super(TestLogin, self).setUp()
+        super(TestLoginView, self).setUp()
         with self.app.test_request_context():
             db.session.add(User('Testing', self.auth_methods[0].name))
             db.session.commit()
 
-    def test_login(self):
+    def test_login(self, username='Testing'):
         client = self.app.test_client()
         data = {
-            'name': 'Testing',
+            'name': username,
             'submit': 'true',
         }
         auth_method = self.auth_methods[0]
@@ -88,17 +40,15 @@ class TestLogin(TestNullAuth):
                 data.items()}
         resp = client.post('/login/', follow_redirects=True, data=data)
         self.assertIn(b'Log Out', resp.data)
-        # For other tests depending on logging in
-        return client
 
     def test_logout(self):
         # Get a test client that's logged in
-        client = self.test_login()
+        client = self.login()
         resp = client.get('/logout/', follow_redirects=True)
         self.assertIn(b'Log In', resp.data)
 
 
-class TestMethodTabs(TestNullAuth):
+class TestMethodTabs(TestLogin):
 
     def test_single_auth_method(self):
         # Remove the second auth method
