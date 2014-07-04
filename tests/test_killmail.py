d5c05c225dd693ebdb1192165494a009064a5b9e
Will Ross <paxswill@gmail.com>
2014-07-04 13:22:14 -0400

    Use explicit unicode for testing the killmails

--- tests/test_killmail.py
+++ tests/test_killmail.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import
-from __future__ import unicode_literals
 from unittest import TestCase
 from decimal import Decimal
 from httmock import HTTMock, all_requests
@@ -11,10 +10,10 @@ class TestKillmail(TestCase):
 
     def test_default_values(self):
         km = killmail.Killmail()
-        attrs = ('kill_id', 'ship_id', 'ship', 'pilot_id', 'pilot',
-            'corp_id', 'corp', 'alliance_id', 'alliance', 'verified',
-            'url', 'value', 'timestamp', 'system', 'constellation',
-            'region')
+        attrs = (u'kill_id', u'ship_id', u'ship', u'pilot_id', u'pilot',
+            u'corp_id', u'corp', u'alliance_id', u'alliance', u'verified',
+            u'url', u'value', u'timestamp', u'system', u'constellation',
+            u'region')
         for attr in attrs:
             self.assertIsNone(getattr(km, attr))
 
@@ -24,7 +23,7 @@ class TestKillmail(TestCase):
         km.foo = 'bar'
         new_dir = dir(km)
         self.assertEqual(old_dir, new_dir)
-        self.assertIn('foo', km._data)
+        self.assertIn(u'foo', km._data)
         self.assertEqual(km.foo, 'bar')
 
     def test_keyword_arguments(self):
@@ -40,7 +39,7 @@ class TestNameMixin(TestCase):
 
     def test_devoter_id(self):
         km = self.NameMixed(ship_id=12017)
-        self.assertEqual(km.ship, 'Devoter')
+        self.assertEqual(km.ship, u'Devoter')
 
 
 class TestLocationMixin(TestCase):
@@ -51,15 +50,15 @@ class TestLocationMixin(TestCase):
 
     def test_system(self):
         km = self.LocationMixed(system_id=30000142)
-        self.assertEqual(km.system, 'Jita')
+        self.assertEqual(km.system, u'Jita')
 
     def test_constellation(self):
         km = self.LocationMixed(system_id=30000142)
-        self.assertEqual(km.constellation, 'Kimotoro')
+        self.assertEqual(km.constellation, u'Kimotoro')
 
     def test_region(self):
         km = self.LocationMixed(system_id=30000142)
-        self.assertEqual(km.region, 'The Forge')
+        self.assertEqual(km.region, u'The Forge')
 
 
 class TestRequestsMixin(TestCase):
@@ -85,49 +84,49 @@ class TestZKillmail(TestCase):
             # Actual testing
             km = killmail.ZKillmail('https://zkillboard.com/kill/37637533/')
             expected_values = {
-                'pilot': 'Paxswill',
-                'ship': 'Devoter',
-                'corp': 'Dreddit',
-                'alliance': 'Test Alliance Please Ignore',
-                'system': 'TA3T-3',
-                'domain': 'zkillboard.com',
-                'value': Decimal('273816945.63'),
+                u'pilot': u'Paxswill',
+                u'ship': u'Devoter',
+                u'corp': u'Dreddit',
+                u'alliance': u'Test Alliance Please Ignore',
+                u'system': u'TA3T-3',
+                u'domain': u'zkillboard.com',
+                u'value': Decimal('273816945.63'),
             }
             for attr, value in expected_values.items():
                 self.assertEqual(getattr(km, attr), value,
-                        msg='{} is not {}'.format(attr, value))
+                        msg=u'{} is not {}'.format(attr, value))
 
     def test_no_alliance_killmail(self):
         with HTTMock(*all_mocks):
             # Actual testing
             km = killmail.ZKillmail('https://zkillboard.com/kill/38862043/')
             expected_values = {
-                'pilot': 'Dave Duclas',
-                'ship': 'Breacher',
-                'corp': 'Omega LLC',
-                'alliance': None,
-                'system': 'Onatoh',
-                'domain': 'zkillboard.com',
-                'value': Decimal('10432408.70'),
+                u'pilot': u'Dave Duclas',
+                u'ship': u'Breacher',
+                u'corp': u'Omega LLC',
+                u'alliance': None,
+                u'system': u'Onatoh',
+                u'domain': u'zkillboard.com',
+                u'value': Decimal('10432408.70'),
             }
             for attr, value in expected_values.items():
                 self.assertEqual(getattr(km, attr), value,
-                        msg='{} is not {}'.format(attr, value))
+                        msg=u'{} is not {}'.format(attr, value))
 
     def test_invalid_zkb_url(self):
         with self.assertRaisesRegexp(ValueError,
-                "'.*' is not a valid zKillboard killmail"):
+                u"'.*' is not a valid zKillboard killmail"):
             killmail.ZKillmail('foobar')
 
     def test_invalid_zkb_response(self):
         @all_requests
         def bad_response(url, request):
-            return response(status_code=403, content='')
+            return response(status_code=403, content=u'')
 
         with HTTMock(bad_response):
             url = 'https://zkillboard.com/kill/38862043/'
             with self.assertRaisesRegexp(LookupError,
-                    "Error retrieving killmail data:.*"):
+                    u"Error retrieving killmail data:.*"):
                 killmail.ZKillmail(url)
 
     def test_invalid_kill_ids(self):
@@ -137,7 +136,7 @@ class TestZKillmail(TestCase):
 
         with HTTMock(empty_response):
             url = 'https://zkillboard.com/kill/0/'
-            with self.assertRaisesRegexp(LookupError, "Invalid killmail: .*"):
+            with self.assertRaisesRegexp(LookupError, u"Invalid killmail: .*"):
                 killmail.ZKillmail(url)
 
 
@@ -149,33 +148,33 @@ class TestCRESTmail(TestCase):
                     'killmails/30290604/'
                     '787fb3714062f1700560d4a83ce32c67640b1797/')
             expected_values = {
-                'pilot': 'CCP FoxFour',
-                'ship': 'Capsule',
-                'corp': 'C C P',
-                'alliance': 'C C P Alliance',
-                'system': 'Todifrauan',
+                u'pilot': u'CCP FoxFour',
+                u'ship': u'Capsule',
+                u'corp': u'C C P',
+                u'alliance': u'C C P Alliance',
+                u'system': u'Todifrauan',
             }
             for attr, value in expected_values.items():
                 self.assertEqual(getattr(km, attr), value,
-                        msg='{} is not {}'.format(attr, value))
+                        msg=u'{} is not {}'.format(attr, value))
 
     def test_invalid_crest_url(self):
         with self.assertRaisesRegexp(ValueError,
-                "'.*' is not a valid CREST killmail"):
+                u"'.*' is not a valid CREST killmail"):
             killmail.CRESTMail('foobar')
 
     def test_invalid_crest_response(self):
         @all_requests
         def bad_hash(url, request):
             return response(
-                content=('{"message": "Invalid killmail ID or hash",'
-                        '"isLocalized": false, "key": "noSuchKill",'
-                        '"exceptionType": "ForbiddenError"}').encode('utf-8'),
+                content=(u'{"message": "Invalid killmail ID or hash",'
+                        u'"isLocalized": false, "key": "noSuchKill",'
+                        u'"exceptionType": "ForbiddenError"}').encode('utf-8'),
                 status_code=403)
 
         with HTTMock(bad_hash):
             url = ''.join(('http://public-crest.eveonline.com/killmails/',
                     '30290604/787fb3714062f1700560d4a83ce32c67640b1797/'))
             with self.assertRaisesRegexp(LookupError,
-                    "Error retrieving CREST killmail:.*"):
+                    u"Error retrieving CREST killmail:.*"):
                 killmail.CRESTMail(url)
