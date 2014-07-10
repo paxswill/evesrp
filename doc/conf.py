2d06d676750c2218ecc74daea2e7167b59d369c1
Will Ross <paxswill@gmail.com>
2014-07-10 01:58:06 -0400

    Use ReadTheDocs' theme for the documentation

--- doc/conf.py
+++ doc/conf.py
@@ -128,9 +128,12 @@ autodoc_default_flags = ['members', 'special-members']
 
 # -- Options for HTML output ----------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'nature'
+# Use ReadTheDocs' theme locally, and it defaults to it on their site
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
