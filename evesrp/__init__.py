7aeac0a7449f399cfa13810a70332245de4e6d3b
Will Ross <paxswill@gmail.com>
2014-07-22 18:57:16 -0400

    Add automatic explicit naming of constraints
    Indexes are skipped, as they already had the default naming scheme applied. SQLite is not supported for this migration, as it makes any migration a pain.

--- evesrp/__init__.py
+++ evesrp/__init__.py
@@ -3,11 +3,31 @@ import locale
 import os
 import requests
 from flask import Flask, current_app, g
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext import sqlalchemy
 from flask.ext.wtf.csrf import CsrfProtect
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import MetaData
 
 
-db = SQLAlchemy()
+db = sqlalchemy.SQLAlchemy()
+# Patch Flask-SQLAlchemy to use a custom Metadata instance with a naming scheme
+# for constraints.
+def _patch_metadata():
+    naming_convention = {
+        'fk': ('fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s'
+                '_%(referred_column_0_name)s'),
+        'pk': 'pk_%(table_name)s',
+        'ix': 'ix_%(table_name)s_%(column_0_name)s',
+        'ck': 'ck_%(table_name)s_%(constraint_name)s',
+        'uq': 'uq_%(table_name)s_%(column_0_name)s',
+    }
+    metadata = MetaData(naming_convention=naming_convention)
+    base = declarative_base(cls=sqlalchemy.Model, name='Model',
+                            metaclass=sqlalchemy._BoundDeclarativeMeta,
+                            metadata=metadata)
+    base.query = sqlalchemy._QueryProperty(db)
+    db.Model = base
+_patch_metadata()
 
 
 from .util import DB_STATS, AcceptRequest
