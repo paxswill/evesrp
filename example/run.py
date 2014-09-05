07e7937dede865707cde3eaf51d1374be3ac1994
Will Ross <paxswill@gmail.com>
2014-09-05 16:26:59 -0400

    Make the example run.py file executable

--- example/run.py
+++ example/run.py
@@ -1,6 +1,10 @@
-import os
+#!/usr/bin/env python
 import os.path
 from evesrp import create_app
 
 
 app = create_app(instance_path=os.path.abspath(os.path.dirname(__file__)))
+
+
+if __name__ == '__main__':
+    app.run()
