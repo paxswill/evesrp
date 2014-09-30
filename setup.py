b2b4dbdc4c617e43a11b6404890c6c9794cb5066
Will Ross <paxswill@gmail.com>
2014-09-29 21:02:27 -0400

    Upgrade other dependencies to latest versions

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     ],
     install_requires=[
         'Flask>=0.10.1',
-        'Flask-Login==0.2.10',
+        'Flask-Login==0.2.11',
         'Flask-Migrate>=1.2.0',
         'Flask-Script==2.0.5',
         'Flask-SQLAlchemy==2.0',
