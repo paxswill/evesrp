4039884c0f2d6ec6c249cc180e3ce6d941641b87
Will Ross <paxswill@gmail.com>
2015-01-03 22:13:06 -0500

    Add corp and alliance groups for Eve SSO

--- evesrp/auth/evesso.py
+++ evesrp/auth/evesso.py
@@ -1,4 +1,6 @@
 from __future__ import absolute_import
+
+import xml.etree.ElementTree as ET
 from flask import request, current_app, abort
 from sqlalchemy.orm.exc import NoResultFound
 from flask.ext.wtf import Form
@@ -22,11 +24,13 @@ class EveSSO(OAuthMethod):
                 'public': 'https://public-crest.eveonline.com/',
                 'authed': 'https://crest-tq.eveonline.com/',
             }
+            self.xml_root = 'https://api.eveonline.com/'
         else:
             self.root_urls = {
                 'public': 'http://public-crest-sisi.testeveonline.com/',
                 'authed': 'https://api-sisi.testeveonline.com/',
             }
+            self.xml_root = 'https://api.testeveonline.com/'
         # Discover the OAuth endpoints by looking in the CREST root
         public_resp = current_app.requests_session.get(
                 self.root_urls['public'])
@@ -96,8 +100,63 @@ class EveSSO(OAuthMethod):
         return [pilot]
 
     def get_groups(self, token):
-        # No groups for Eve SSO. Maybe sometime in the future we'll be able to
-        # use SSO as a restricted way to get access to the rest of the API,
-        # like to look up a user's mailing lists and then create groups from
-        # that.
-        return []
+        """Set the user's groups for their pilot.
+
+        At this time, Eve SSO only gives us character access, so they're just
+        set to the pilot's corporation, and if they have on their alliance as
+        well. In the future, this method may also add groups for mailing lists.
+        """
+        character = self._get_user_data(token)
+        info_url = self.xml_root + 'eve/CharacterInfo.xml.aspx'
+        info_response = current_app.requests_session.get(info_url,
+                params={'characterID': character['id']})
+        api_tree = ET.fromstring(info_response.text).find('result')
+        corp_name = api_tree.find('corporation')
+        corp_id = api_tree.find('corporationID')
+        corporation = {
+            'name': corp_name.text,
+            'id': int(corp_id.text),
+        }
+        try:
+            corp_group = EveSSOGroup.query.filter_by(alliance=False,
+                    ccp_id=int(corp_id.text),
+                    authmethod=self.name).one()
+        except NoResultFound:
+            corp_group = EveSSOGroup(corp_name.text, int(corp_id.text), False,
+                    self.name)
+            db.session.add(corp_group)
+        groups = [corp_group]
+
+        alliance_name = api_tree.find('alliance')
+        alliance_id = api_tree.find('allianceID')
+        # If there's an alliance, set it up
+        if alliance_name is not None and alliance_id is not None:
+            try:
+                alliance_group = EveSSOGroup.query.filter_by(alliance=True,
+                        ccp_id=int(alliance_id.text),
+                        authmethod=self.name).one()
+            except NoResultFound:
+                alliance_group = EveSSOGroup(alliance_name.text,
+                        int(alliance_id.text), True, self.name)
+                db.session.add(alliance_group)
+        db.session.commit()
+        return groups
+
+
+class EveSSOGroup(Group):
+
+    id = db.Column(db.Integer, db.ForeignKey(Group.id), primary_key=True)
+
+    ccp_id = db.Column(db.Integer, nullable=False, unique=True, index=True)
+
+    alliance = db.Column(db.Boolean, nullable=False, default=False,
+            index=True)
+
+    __table_args__ = (
+            db.UniqueConstraint(ccp_id, alliance),
+    )
+
+    def __init__(self, name, ccp_id, alliance, authmethod, **kwargs):
+        self.ccp_id = ccp_id
+        self.alliance = alliance
+        super(EveSSOGroup, self).__init__(name, authmethod, **kwargs)
