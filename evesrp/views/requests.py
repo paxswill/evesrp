5097d0550c6aab241c37816621b10adc6957d893
Will Ross <paxswill@gmail.com>
2015-04-27 19:27:37 -0400

    Add the sum of payouts to the XML API
    Closes #78. Two lines, so sue me.

--- evesrp/views/requests.py
+++ evesrp/views/requests.py
@@ -311,7 +311,8 @@ class RequestListing(View):
                     main_link=url_for(request.endpoint, filters=filters,
                         _external=True))
         if request.is_xml:
-            return xmlify('request_list.xml', requests=pager.items)
+            return xmlify('request_list.xml', requests=pager.items,
+                    total_payouts=total_payouts)
         if 'title' in kwargs:
             title = kwargs.pop('title')
         else:
