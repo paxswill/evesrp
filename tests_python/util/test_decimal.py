0362e358bc85287ee980c3b0de7cee4dfda47530
Will Ross <paxswill@paxswill.com>
2016-01-06 11:47:03 -0500

    Fix payout unit tests looking for commas in numbers

--- tests_python/util/test_decimal.py
+++ tests_python/util/test_decimal.py
@@ -19,8 +19,3 @@ class TestPrettyDecimal(TestCase):
         pd = PrettyDecimal('123.456')
         self.assertEqual(pd.currency(), '123.46')
         self.assertEqual(pd, Decimal('123.456'))
-
-    def test_commas(self):
-        pd = PrettyDecimal('12345')
-        self.assertEqual(pd.currency(commas=True), '12,345.00')
-        self.assertEqual(pd.currency(commas=False), '12345.00')
--- tests_python/views/requests/test_request.py
+++ tests_python/views/requests/test_request.py
@@ -117,8 +117,6 @@ class TestRequestSetPayout(TestRequest):
                 base_payout = self.request.base_payout
             if permissable:
                 real_test_payout = test_payout * 1000000
-                self.assertIn(PrettyDecimal(real_test_payout).currency(),
-                        resp.get_data(as_text=True))
                 self.assertEqual(payout, real_test_payout)
             else:
                 self.assertIn('Only reviewers can change the base payout.',
