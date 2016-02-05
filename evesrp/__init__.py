1f0381eb627b3db2d44742f090fb7689b6312fe9
Will Ross <paxswill@paxswill.com>
2016-02-04 23:23:23 -0500

    Start v0.11.3 development

--- evesrp/__init__.py
+++ evesrp/__init__.py
@@ -56,7 +56,7 @@ _patch_httplib()
 from .util import DB_STATS, AcceptRequest, WeakCiphersAdapter
 
 
-__version__ = u'0.11.2-dev'
+__version__ = u'0.11.3-dev'
 
 
 requests_session = requests.Session()
