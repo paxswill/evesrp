e7d3d2813d10c2abd067164d69ca03370075c1ad
Will Ross <paxswill@paxswill.com>
2016-05-23 00:07:22 -0400

    Only add flashed messages for XHR requests.

--- src/evesrp/util/jsonify.py
+++ src/evesrp/util/jsonify.py
@@ -2,6 +2,8 @@ import flask
 
 
 def jsonify(*args, **kwargs):
-    messages = [{'message': m, 'category':c} for c, m in
-            flask.get_flashed_messages(with_categories=True)]
-    return flask.jsonify(*args, flashed_messages=messages, **kwargs)
+    if flask.request.is_xhr:
+        # Add flashed messages
+        kwargs[u'flashed_messages'] = [{'message': m, 'category':c} for c, m in
+                flask.get_flashed_messages(with_categories=True)]
+    return flask.jsonify(*args, **kwargs)
