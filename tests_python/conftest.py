7f9cfb3a4f711ca2b34bf275b66ebc6b4dc79e24
Will Ross <paxswill@paxswill.com>
2016-07-16 14:58:10 -0400

    Rewrite test_models.py to use py.test

--- tests_python/conftest.py
+++ tests_python/conftest.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+import datetime as dt
 from os import environ as env
 
 import pytest
@@ -7,8 +8,10 @@ from flask import redirect, url_for, request, render_template
 from flask_wtf import Form
 from wtforms.fields import StringField, SubmitField
 from evesrp import create_app, db
-from evesrp.auth import AuthMethod
-from evesrp.auth.models import User
+from evesrp.models import Request, AbsoluteModifier
+from evesrp.auth import AuthMethod, PermissionType
+from evesrp.auth.models import User, Division, Permission, Pilot
+from evesrp.util import utc
 from . import mocks
 
 
@@ -150,3 +153,37 @@ def crest():
 @pytest.fixture
 def zkillboard():
     return mocks.zKillboard
+
+
+@pytest.fixture
+def srp_request(user, other_user):
+    mock_killmail = dict(
+            id=12842852,
+            type_name='Erebus',
+            type_id=671,
+            corporation='Ever Flow',
+            corporation_id=1991488321,
+            alliance='Northern Coalition.',
+            alliance_id=1727758877,
+            killmail_url='http://eve-kill.net/?a=kill_detail&kll_id=12842852',
+            base_payout=73957900000,
+            kill_timestamp=dt.datetime(2012, 3, 25, 0, 44, 0, tzinfo=utc),
+            system='92D-OI',
+            system_id=30001312,
+            constellation='XHYS-O',
+            constellation_id=20000191,
+            region='Venal',
+            region_id=10000015,
+            pilot_id=133741,
+    )
+    division = Division('Testing Disivion')
+    # SQLAlchemy magic is adding these to the database session
+    Pilot(user, 'eLusi0n', mock_killmail['pilot_id'])
+    srp_request = Request(user, 'Original details', division,
+                          mock_killmail.items())
+    Permission(division, PermissionType.review, other_user)
+    Permission(division, PermissionType.pay, other_user)
+    # Modifier used by some tests.
+    AbsoluteModifier(srp_request, other_user, 'Absolute Fixture Modifier', 10)
+    db.session.commit()
+    return srp_request
--- tests_python/test_models.py
+++ tests_python/test_models.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import unicode_literals
 import datetime as dt
 from decimal import Decimal
+import pytest
 from .util_tests import TestLogin
 from evesrp import db
 from evesrp.models import ActionType, ActionError, Action, Request,\
@@ -11,186 +12,110 @@ from evesrp.auth.models import Pilot, Division, Permission
 from evesrp.util import utc
 
 
-class TestModels(TestLogin):
+@pytest.fixture(params=ActionType.statuses, ids=(lambda s: s.value))
+def request_status(request, srp_request, other_user):
+    # Skip if the status already matches
+    if request.param == srp_request.status:
+        return request.param
+    # For paid status, we need to be approved first
+    if request.param == ActionType.paid:
+        Action(srp_request, other_user, type_=ActionType.approved)
+    Action(srp_request, other_user, type_=request.param)
+    db.session.commit()
+    assert srp_request.status == request.param
+    return request.param
 
-    def setUp(self):
-        super(TestModels, self).setUp()
-        with self.app.test_request_context():
-            div = Division('Testing Division')
-            mock_killmail = dict(
-                    id=12842852,
-                    type_name='Erebus',
-                    type_id=671,
-                    corporation='Ever Flow',
-                    corporation_id=1991488321,
-                    alliance='Northern Coalition.',
-                    alliance_id=1727758877,
-                    killmail_url=('http://eve-kill.net/?a=kill_detail'
-                        '&kll_id=12842852'),
-                    base_payout=73957900000,
-                    kill_timestamp=dt.datetime(2012, 3, 25, 0, 44, 0,
-                        tzinfo=utc),
-                    system='92D-OI',
-                    system_id=30001312,
-                    constellation='XHYS-O',
-                    constellation_id=20000191,
-                    region='Venal',
-                    region_id=10000015,
-                    pilot_id=133741,
-            )
-            Pilot(self.normal_user, 'eLusi0n', 133741)
-            Request(self.normal_user, 'Original details', div,
-                    mock_killmail.items())
-            Permission(div, PermissionType.review, self.admin_user)
-            Permission(div, PermissionType.pay, self.admin_user)
+
+# Just a way to test success and failure of permissions
+@pytest.fixture(params=[True, False], ids=['user', 'other_user'])
+def a_user(request, user, other_user):
+    if request.param:
+        return user
+    else:
+        return other_user
+
+
+class TestModifiers(object):
+
+    def test_add_modifier(self, srp_request, a_user, request_status):
+        status_success = request_status == ActionType.evaluating
+        permissions_success = 'Other' in a_user.name
+        start_payout = srp_request.payout
+        if status_success and permissions_success:
+            AbsoluteModifier(srp_request, a_user, '', 10)
             db.session.commit()
-
-    @property
-    def request(self):
-        return Request.query.first()
-
-    def add_action(self, type_):
-        action = Action(self.request, self.admin_user, type_=type_)
-        db.session.commit()
-        return action.id
-
-    def add_modifier(self, value, absolute=True):
-        if absolute:
-            modifier = AbsoluteModifier(self.request, self.admin_user, None,
-                    Decimal(value))
+            assert srp_request.payout == start_payout + 10
         else:
-            modifier = RelativeModifier(self.request, self.admin_user, None,
-                    Decimal(value))
-        db.session.add(modifier)
+            with pytest.raises(ModifierError) as excinfo:
+                AbsoluteModifier(srp_request, a_user, '', 10)
+                db.session.commit()
+
+    def test_void_modifier(self, srp_request, a_user, request_status):
+        status_success = request_status == ActionType.evaluating
+        permissions_success = 'Other' in a_user.name
+        start_payout = srp_request.payout
+        modifier = srp_request.modifiers[0]
+        if status_success and permissions_success:
+            modifier.void(a_user)
+            db.session.commit()
+            assert srp_request.payout == srp_request.base_payout
+        else:
+            with pytest.raises(ModifierError) as excinfo:
+                modifier.void(a_user)
+                db.session.commit()
+
+
+class TestActionStatus(object):
+
+    def test_default_status(self, srp_request):
+        assert srp_request.status == ActionType.evaluating
+
+    @pytest.fixture(params=ActionType.statuses, ids=(lambda s: s.value))
+    def next_status(self, request):
+        return request.param
+
+    # Also implicitly testing the setting of Request.status
+    def test_state_machine(self, srp_request, other_user, request_status,
+                           next_status):
+        success = next_status in Request.state_rules[request_status]
+        if success:
+            Action(srp_request, other_user, type_=next_status)
+            db.session.commit()
+            assert srp_request.status == next_status
+        else:
+            with pytest.raises(ActionError) as excinfo:
+                Action(srp_request, other_user, type_=next_status)
+                db.session.commit()
+            assert srp_request.status == request_status
+
+
+class TestDelete_new(object):
+
+    def test_delete_action(self, srp_request, other_user):
+        action = Action(srp_request, other_user, type_=ActionType.approved)
         db.session.commit()
-        return modifier.id
+        db.session.delete(action)
+        db.session.commit()
+        db.session.expire_all()
+        assert srp_request is not None
 
+    def test_delete_modifier(self, srp_request):
+        modifier = srp_request.modifiers[0]
+        db.session.delete(modifier)
+        db.session.commit()
+        db.session.expire_all()
+        assert srp_request is not None
 
-class TestModifiers(TestModels):
-
-    def test_add_modifier(self):
-        with self.app.test_request_context():
-            start_payout = self.request.payout
-            AbsoluteModifier(self.request, self.admin_user, '', 10)
-            db.session.commit()
-            self.assertEqual(self.request.payout, start_payout + 10)
-        return start_payout
-
-    def test_void_modifier(self):
-        start_payout = self.test_add_modifier()
-        with self.app.test_request_context():
-            mod = self.request.modifiers[0].void(self.admin_user)
-            db.session.commit()
-            self.assertEqual(self.request.payout, start_payout)
-
-    def test_add_modifier_bad_status(self):
-        with self.app.test_request_context():
-            self.add_action(ActionType.approved)
-            with self.assertRaises(ModifierError):
-                AbsoluteModifier(self.request, self.admin_user, '', 10)
-                db.session.commit()
-
-    def test_void_modifier_bad_status(self):
-        start_payout = self.test_add_modifier()
-        with self.app.test_request_context():
-            self.add_action(ActionType.approved)
-            mod = self.request.modifiers[0]
-            with self.assertRaises(ModifierError):
-                mod.void(self.admin_user)
-                db.session.commit()
-            self.assertNotEqual(self.request.payout, start_payout)
-
-    def test_add_modifier_bad_permissions(self):
-        with self.app.test_request_context():
-            with self.assertRaises(ModifierError):
-                AbsoluteModifier(self.request, self.normal_user, '', 10)
-                db.session.commit()
-
-    def test_void_modifier_bad_permissions(self):
-        start_payout = self.test_add_modifier()
-        with self.app.test_request_context():
-            mod = self.request.modifiers[0]
-            with self.assertRaises(ModifierError):
-                mod.void(self.normal_user)
-                db.session.commit()
-            self.assertNotEqual(self.request.payout, start_payout)
-
-
-class TestActionStatus(TestModels):
-
-    def test_default_status(self):
-        with self.app.test_request_context():
-            # Check default status
-            self.assertEqual(self.request.status, ActionType.evaluating)
-
-    def test_status_updating(self):
-        with self.app.test_request_context():
-            Action(self.request, self.admin_user, type_=ActionType.approved)
-            db.session.commit()
-            self.assertEqual(self.request.status, ActionType.approved)
-
-    def test_status_state_machine(self):
-        with self.app.test_request_context():
-            # To and From evaluating (except from paid)
-            for action in (ActionType.incomplete, ActionType.rejected,
-                    ActionType.approved):
-                self.add_action(action)
-                self.assertEqual(self.request.status, action)
-                self.add_action(ActionType.evaluating)
-                self.assertEqual(self.request.status, ActionType.evaluating)
-            with self.assertRaises(ActionError):
-                self.add_action(ActionType.paid)
-            # From incomplete
-            self.add_action(ActionType.incomplete)
-            self.add_action(ActionType.rejected)
-            self.assertEqual(self.request.status, ActionType.rejected)
-            # From rejected
-            for action in ActionType.statuses.difference(
-                    (ActionType.evaluating,)):
-                with self.assertRaises(ActionError):
-                    self.add_action(action)
-            self.add_action(ActionType.evaluating)
-            # From approved
-            self.add_action(ActionType.approved)
-            for action in (ActionType.incomplete, ActionType.rejected):
-                with self.assertRaises(ActionError):
-                    self.add_action(action)
-            self.add_action(ActionType.paid)
-            self.assertEqual(self.request.status, ActionType.paid)
-            # From paid
-            for action in (ActionType.incomplete, ActionType.rejected):
-                with self.assertRaises(ActionError):
-                    self.add_action(action)
-            self.add_action(ActionType.approved)
-            self.assertEqual(self.request.status, ActionType.approved)
-            self.add_action(ActionType.paid)
-            self.add_action(ActionType.evaluating)
-            self.assertEqual(self.request.status, ActionType.evaluating)
-
-
-class TestDelete(TestModels):
-
-    def test_delete_action(self):
-        with self.app.test_request_context():
-            aid = self.add_action(ActionType.approved)
-            db.session.delete(Action.query.get(aid))
-            db.session.commit()
-            self.assertIsNotNone(self.request)
-
-    def test_delete_modifier(self):
-        with self.app.test_request_context():
-            mid = self.add_modifier(10)
-            db.session.delete(Modifier.query.get(mid))
-            db.session.commit()
-            self.assertIsNotNone(self.request)
-
-    def test_delete_request(self):
-        with self.app.test_request_context():
-            rid = self.request.id
-            mid = self.add_modifier(10)
-            aid = self.add_action(ActionType.approved)
-            db.session.delete(self.request)
-            db.session.commit()
-            self.assertIsNone(Modifier.query.get(mid))
-            self.assertIsNone(Action.query.get(aid))
-            self.assertIsNone(self.request)
+    def test_delete_request(self, srp_request, other_user):
+        action = Action(srp_request, other_user, type_=ActionType.approved)
+        modifier = srp_request.modifiers[0]
+        db.session.commit()
+        action_id = action.id
+        modifier_id = modifier.id
+        request_id = srp_request.id
+        db.session.delete(srp_request)
+        db.session.commit()
+        db.session.expire_all()
+        assert AbsoluteModifier.query.get(modifier_id) is None
+        assert Action.query.get(action_id) is None
+        assert Request.query.get(request_id) is None
