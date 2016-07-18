c0a41d79a5253b28bce0434a2b72185b63a7ee12
Will Ross <paxswill@paxswill.com>
2016-07-18 11:28:44 -0400

    Rewrite auth/test_models.py to use py.test

--- tests_python/auth/test_models.py
+++ tests_python/auth/test_models.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
+import pytest
 from ..util_tests import TestApp
-from ..test_models import TestModels
 from evesrp import db
 from evesrp.models import Request, Modifier, Action, ActionType
 from evesrp.auth import PermissionType
@@ -9,248 +9,142 @@ from evesrp.auth.models import Entity, User, Group, Permission, Division,\
     PermissionType, APIKey, Note
 
 
-class TestGroups(TestApp):
-
-    def setUp(self):
-        super(TestGroups, self).setUp()
-        with self.app.test_request_context():
-            u1 = User('User One', 'AuthMethod', id=1)
-            u2 = User('User Two', 'AuthMethod', id=2)
-            u3 = User('User Three', 'AuthMethod', id=3)
-            u4 = User('User Four', 'AuthMethod', id=4)
-            g1 = Group('Group One', 'AuthMethod', id=10)
-            g2 = Group('Group Two', 'AuthMethod', id=20)
-            db.session.add_all((u1, u2, u3, u4, g1, g2))
-            # Users 1 and 2 belong to Groups 1 and 2. User 3 belongs to both,
-            # User 4 belongs to no groups
-            g1.users.add(u1)
-            g2.users.add(u2)
-            g1.users.add(u3)
-            g2.users.add(u3)
-            db.session.commit()
-
-    def test_group_membership(self):
-        with self.app.test_request_context():
-            self.assertIn(
-                    User.query.get(1),
-                    Group.query.get(10).users)
-            self.assertNotIn(
-                    User.query.get(1),
-                    Group.query.get(20).users)
-            self.assertIn(
-                    User.query.get(2),
-                    Group.query.get(20).users)
-            self.assertNotIn(
-                    User.query.get(2),
-                    Group.query.get(10).users)
-            self.assertIn(
-                    User.query.get(3),
-                    Group.query.get(10).users)
-            self.assertIn(
-                    User.query.get(3),
-                    Group.query.get(20).users)
-            self.assertNotIn(
-                    User.query.get(4),
-                    Group.query.get(10).users)
-            self.assertNotIn(
-                    User.query.get(4),
-                    Group.query.get(20).users)
+@pytest.fixture
+def make_group(evesrp_app):
+    def _make_group(name):
+        group = Group(name, 'AuthMethod')
+        db.session.add(group)
+        db.session.commit()
+        return group
+    return _make_group
 
 
-class TestPermissions(TestGroups):
-
-    def setUp(self):
-        super(TestPermissions, self).setUp()
-        with self.app.test_request_context():
-            # Get the entities
-            u1 = User.query.get(1)
-            u2 = User.query.get(2)
-            u3 = User.query.get(3)
-            u4 = User.query.get(4)
-            g1 = Group.query.get(10)
-            g2 = Group.query.get(20)
-            # Three Divisions, Even, Odd, and Prime
-            even = Division('Even')
-            odd = Division('Odd')
-            prime = Division('Prime')
-            db.session.add_all((even, odd, prime))
-            # Grant submit for the even, odd and prime groups to the
-            # individual users (except user 4)
-            Permission(odd, PermissionType.submit, u1)
-            Permission(even, PermissionType.submit, u2)
-            Permission(odd, PermissionType.submit, u3)
-            Permission(prime, PermissionType.submit, u2)
-            Permission(prime, PermissionType.submit, u3)
-            # Grant review for odd to group 1
-            Permission(odd, PermissionType.review, g1)
-            # Grant pay to user 4 for prime and to user 3 for even
-            Permission(even, PermissionType.pay, u3)
-            Permission(prime, PermissionType.pay, u4)
-            # In summary:
-            # User 1 can submit to odd, and review odd (through Group 1)
-            # User 2 can submit to even and prime
-            # User 3 can submit to odd and prime, review odd (through Group
-            # 1), and pay out even
-            # User 4 can pay out prime
-            db.session.commit()
-
-    def test_basic_has_permission(self):
-        with self.app.test_request_context():
-            # Get users
-            u1 = User.query.get(1)
-            u2 = User.query.get(2)
-            u3 = User.query.get(3)
-            u4 = User.query.get(4)
-            # Actual tests
-            self.assertTrue(u1.has_permission(PermissionType.submit))
-            self.assertTrue(u1.has_permission(PermissionType.review))
-            self.assertFalse(u1.has_permission(PermissionType.pay))
-
-            self.assertTrue(u2.has_permission(PermissionType.submit))
-            self.assertFalse(u2.has_permission(PermissionType.review))
-            self.assertFalse(u2.has_permission(PermissionType.pay))
-
-            self.assertTrue(u3.has_permission(PermissionType.submit))
-            self.assertTrue(u3.has_permission(PermissionType.review))
-            self.assertTrue(u3.has_permission(PermissionType.pay))
-
-            self.assertFalse(u4.has_permission(PermissionType.submit))
-            self.assertFalse(u4.has_permission(PermissionType.review))
-            self.assertTrue(u4.has_permission(PermissionType.pay))
-
-    def test_has_permission_in_division(self):
-        with self.app.test_request_context():
-            # Get users
-            u1 = User.query.get(1)
-            u2 = User.query.get(2)
-            u3 = User.query.get(3)
-            u4 = User.query.get(4)
-            odd = Division.query.filter_by(name='Odd').one()
-            even = Division.query.filter_by(name='Even').one()
-            prime = Division.query.filter_by(name='Prime').one()
-            # Tests
-            users = (u1, u2, u3, u4)
-            divisions = (odd, even, prime)
-            permissions = PermissionType.all
-            passing = {
-                u1: {
-                    odd: (PermissionType.submit, PermissionType.review),
-                },
-                u2: {
-                    even: (PermissionType.submit,),
-                    prime: (PermissionType.submit,),
-                },
-                u3: {
-                    odd: (PermissionType.submit, PermissionType.review),
-                    even: (PermissionType.pay,),
-                    prime: (PermissionType.submit,),
-                },
-                u4: {
-                    prime: (PermissionType.pay,),
-                },
-            }
-            for user in users:
-                # every user has an entry in passing, so don't bother checking
-                # for existence
-                passing_divisions = passing[user]
-                false_negative = ''.join(("{user.name} does have {perm} "
-                                          " in {div.name}."))
-                false_positive = ''.join(("{user.name} does not have "
-                                          "{perm} in {div.name}."))
-                for division in divisions:
-                    # If a division isn't present, the user has no permissions
-                    # for it.
-                    passing_permissions = passing_divisions.get(division)
-                    for permission in permissions:
-                        if passing_permissions is None \
-                                or permission not in passing_permissions:
-                            self.assertFalse(
-                                    user.has_permission(permission, division),
-                                    msg=false_negative.format(
-                                            user=user,
-                                            perm=permission,
-                                            div=division))
-                        else:
-                            self.assertTrue(
-                                    user.has_permission(permission, division),
-                                    msg=false_positive.format(
-                                            user=user,
-                                            perm=permission,
-                                            div=division))
-
-    def test_division_permissions(self):
-        with self.app.test_request_context():
-            # Get users and divisions
-            odd = Division.query.filter_by(name='Odd').one().permissions
-            even = Division.query.filter_by(name='Even').one().permissions
-            prime = Division.query.filter_by(name='Prime').one().permissions
-            # Tests
-            self.assertEqual(len(odd[PermissionType.submit]), 2)
-            self.assertEqual(len(odd[PermissionType.review]), 1)
-            self.assertEqual(len(odd[PermissionType.pay]), 0)
-
-            self.assertEqual(len(even[PermissionType.submit]), 1)
-            self.assertEqual(len(even[PermissionType.review]), 0)
-            self.assertEqual(len(even[PermissionType.pay]), 1)
-
-            self.assertEqual(len(prime[PermissionType.submit]), 2)
-            self.assertEqual(len(prime[PermissionType.review]), 0)
-            self.assertEqual(len(prime[PermissionType.pay]), 1)
+@pytest.fixture
+def make_division(evesrp_app):
+    def _make_division(name):
+        division = Division(name)
+        db.session.add(division)
+        db.session.commit()
+        return division
+    return _make_division
 
 
-class TestDelete(TestModels):
-
-    def test_delete_api_key(self):
-        with self.app.test_request_context():
-            admin_user = self.admin_user
-            api_key = APIKey(admin_user)
-            db.session.add(api_key)
-            db.session.commit()
-            key_id = api_key.id
-            db.session.delete(api_key)
-            db.session.commit()
-            self.assertIsNone(APIKey.query.get(key_id))
-            self.assertIsNotNone(self.admin_user)
-
-    def test_delete_note(self):
-        with self.app.test_request_context():
-            note = Note(self.normal_user, self.admin_user, 'A note.')
-            db.session.add(note)
-            db.session.commit()
-            note_id = note.id
-            db.session.delete(note)
-            db.session.commit()
-            self.assertIsNone(Note.query.get(note_id))
-            self.assertIsNotNone(self.normal_user)
-            self.assertIsNotNone(self.admin_user)
-
-    def test_delete_permission(self):
-        with self.app.test_request_context():
-            review_perm = Permission.query.filter_by(
-                    permission=PermissionType.review,
-                    entity=self.admin_user).one()
-            review_id = review_perm.id
-            division = Division.query.one()
-            division_id = division.id
-            db.session.delete(review_perm)
-            db.session.commit()
-            self.assertIsNone(Permission.query.get(review_id))
-            self.assertIsNotNone(self.admin_user)
-            self.assertIsNotNone(Division.query.get(division_id))
+@pytest.fixture(params=PermissionType.all, ids=(lambda p: p.value))
+def permission(request):
+    return request.param
 
 
-    def test_delete_division(self):
-        with self.app.test_request_context():
-            review_perm = Permission.query.filter_by(
-                    permission=PermissionType.review,
-                    entity=self.admin_user).one()
-            review_id = review_perm.id
-            division = Division.query.one()
-            division_id = division.id
-            request = Request.query.one()
-            request_id = request.id
-            db.session.delete(division)
-            db.session.commit()
-            self.assertIsNone(Division.query.get(division_id))
-            self.assertIsNone(Permission.query.get(review_id))
-            self.assertIsNone(Request.query.get(request_id))
+@pytest.fixture
+def make_permission(evesrp_app, permission):
+    def _make_permission(entity, division):
+        perm = Permission(division, permission, entity)
+        db.session.commit()
+        return perm
+    return _make_permission
+
+
+class TestGroups(object):
+
+
+    def test_group_membership(self, make_group, user):
+        group1 = make_group('One')
+        group2 = make_group('Two')
+        group1.users.add(user)
+        db.session.commit()
+        group1_id = group1.id
+        group2_id = group2.id
+        db.session.expire_all()
+        test_user = User.query.get(user.id)
+        test_group = Group.query.get(group1_id)
+        assert test_user in test_group.users
+        test_group = Group.query.get(group2_id)
+        assert test_user not in test_group.users
+
+
+class TestPermissions(object):
+
+    def test_user_permission(self, user, other_user, make_division,
+                              make_permission, permission):
+        division = make_division('Division A')
+        make_permission(user, division)
+        db.session.commit()
+        assert user.has_permission(permission) 
+        assert not other_user.has_permission(permission) 
+
+    def test_user_division_permission(self, user, other_user, make_division,
+                                      make_permission, permission):
+        division1 = make_division('Division 1')
+        division2 = make_division('Division 2')
+        make_permission(user, division1)
+        make_permission(other_user, division2)
+        db.session.commit()
+        assert user.has_permission(permission, division1) 
+        assert not other_user.has_permission(permission, division1) 
+        assert other_user.has_permission(permission, division2) 
+        assert not user.has_permission(permission, division2)
+
+    def test_group_permission(self, user, other_user, make_group,
+                              make_division, make_permission, permission):
+        group = make_group('Group 1')
+        other_group = make_group('Group 2')
+        group.users.add(user)
+        other_group.users.add(other_user)
+        division = make_division('Division A')
+        make_permission(group, division)
+        db.session.commit()
+        assert group.has_permission(permission)
+        assert not other_group.has_permission(permission)
+        assert user.has_permission(permission) 
+        assert not other_user.has_permission(permission) 
+
+
+@pytest.mark.usefixtures('evesrp_app')
+@pytest.mark.parametrize('user_role', ['Normal'])
+class TestDelete(object):
+
+    def test_delete_api_key(self, user):
+        api_key = APIKey(user)
+        db.session.add(api_key)
+        db.session.commit()
+        key_id = api_key.id
+        db.session.delete(api_key)
+        db.session.commit()
+        assert APIKey.query.get(key_id) is None
+        assert user is not None
+
+    def test_delete_note(self, user, other_user):
+        note = Note(user, other_user, 'A note.')
+        db.session.add(note)
+        db.session.commit()
+        note_id = note.id
+        db.session.delete(note)
+        db.session.commit()
+        assert Note.query.get(note_id) is None
+        assert user is not None
+        assert other_user is not None
+
+    @pytest.mark.parametrize('permission', [PermissionType.review])
+    def test_delete_permission(self, user, make_division, make_permission):
+        division = make_division('A Division')
+        division_id = division.id
+        review_perm = make_permission(user, division)
+        review_id = review_perm.id
+        db.session.delete(review_perm)
+        db.session.commit()
+        assert Permission.query.get(review_id) is None
+        assert user is not None
+        assert Division.query.get(division_id) is not None
+
+    def test_delete_division(self, srp_request):
+        permission = Permission.query.filter_by(
+                permission=PermissionType.review).one()
+        permission_id = permission.id
+        division = Division.query.one()
+        division_id = division.id
+        request = Request.query.one()
+        request_id = request.id
+        db.session.delete(division)
+        db.session.commit()
+        assert Division.query.get(division_id) is None
+        assert Permission.query.get(permission_id) is None
+        assert Request.query.get(request_id) is None
