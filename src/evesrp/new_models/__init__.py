69c04d3d78dbc9e039745b9d1a6950736ce891fc
Will Ross <paxswill@paxswill.com>
2016-12-21 14:39:41 -0500

    Conceal component types in new_models package

---
+++ src/evesrp/new_models/__init__.py
@@ -0,0 +1,3 @@
+from .authorization import Entity, User, Group, Division, Permission, \
+                           PermissionType
+from .request import Pilot, Killmail, Request, Action, Modifier, ActionType
