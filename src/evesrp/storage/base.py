efaf24aa0433a719f650269941eb736debbd7501
Will Ross <paxswill@paxswill.com>
2017-02-15 16:57:32 -0500

    Initial BaseStorage implementation
    Stubbed out almost everything with NotImplementedErrors, except for filter_sparse which is a naive implementation depending on filter_requests.
    
    It currently does handle CCP lookups right now, that’s coming later.

--- src/evesrp/storage/base.py
+++ src/evesrp/storage/base.py
@@ -1,110 +1,230 @@
-class BaseStore(object):
+import six
+from evesrp import search_filter as sfilter
 
-    def __init__(self):
-        pass
+
+class BaseStore(object):
 
     ### Divisions ###
 
     def get_division(self, division_id):
-        pass
+        raise NotImplementedError
+
+    def get_divisions(self, division_ids):
+        raise NotImplementedError
 
     def add_division(self, division):
-        pass
+        raise NotImplementedError
 
     def save_division(self, division):
-        pass
+        raise NotImplementedError
 
     ### Permissions ###
 
     def get_permission(self):
-        pass
+        raise NotImplementedError
 
     def get_permissions(self, **kwargs):
-        pass
+        # entity_id, division_id, types, type_
+        raise NotImplementedError
 
     def add_permission(self, permission):
-        pass
+        raise NotImplementedError
 
     def remove_permission(self, permission_id):
-        pass
+        raise NotImplementedError
 
     ### Users and Groups ###
 
     def get_user(self, user_id):
-        pass
+        raise NotImplementedError
 
-    def get_users(self, **kwargs):
-        pass
+    def get_users(self, group_id):
+        raise NotImplementedError
 
-    def get_groups(self, **kwargs):
-        pass
+    def get_groups(self, user_id):
+        raise NotImplementedError
 
     def associate_user_group(self, user_id, group_id):
-        pass
+        raise NotImplementedError
 
     def disassociate_user_group(self, user_id, group_id):
-        pass
+        raise NotImplementedError
 
     ### Killmails ###
 
     def get_killmail(self, killmail_id):
-        pass
+        raise NotImplementedError
 
-    def get_killmails(self, **kwargs):
-        pass
+    def get_killmails(self, killmail_ids):
+        raise NotImplementedError
 
     ### Requests ###
 
     def get_request(self):
-        pass
+        raise NotImplementedError
 
-    def get_requests(self, **kwargs):
-        pass
+    def get_requests(self, killmail_id):
+        raise NotImplementedError
 
     def add_request(self, request):
-        pass
+        raise NotImplementedError
 
     def save_request(self, request):
-        pass
+        raise NotImplementedError
 
     ### Request Actions ###
 
     def get_action(self):
-        pass
+        raise NotImplementedError
 
-    def get_actions(self, **kwargs):
-        pass
+    def get_actions(self, request_id):
+        raise NotImplementedError
 
     def add_action(self, action):
-        pass
+        raise NotImplementedError
 
     ### Request Modifiers ###
 
     def get_modifier(self, modifier_id):
-        pass
+        raise NotImplementedError
 
     def get_modifiers(self, **kwargs):
-        pass
+        # request_id, void, type_
+        raise NotImplementedError
 
     def add_modifier(self, modifier):
-        pass
+        raise NotImplementedError
 
     def save_modifier(self, modifier):
-        pass
+        raise NotImplementedError
 
     ### Filtering ###
 
     def filter_requests(self, filters):
-        pass
+        raise NotImplementedError
+
+    _request_fields = frozenset((
+        'killmail_id',
+        'division',
+        'division_id',
+        'submit_timestamp',
+        'details',
+        'payout',
+        'base_payout',
+        'status',
+    ))
+
+    _killmail_fields = frozenset((
+        # 'user' is not a field that can be queried in a sparse search because
+        # sparse searches are to be used only for presentation to the user, and
+        # the user shouldn't need to know the specific user behind a request,
+        # only the character.
+        'kill_timestamp',
+        'type',
+        'type_id',
+        'pilot',
+        'pilot_id',
+        'corporation',
+        'corporation_id',
+        'alliance',
+        'alliance_id',
+        'solar_system',
+        'solar_system_id',
+        'constellation',
+        'constellation_id',
+        'region',
+        'region_id',
+    ))
+
+    _combo_fields = {
+        'division': ('division_id', 'division_name'),
+        'type': ('type_id', 'type_name'),
+        'pilot': ('pilot_id', 'pilot_name'),
+        'corporation': ('corporation_id', 'corporation_name'),
+        'alliance': ('alliance_id', 'alliance_name'),
+        'solar_system': ('system_id', 'solar_system_name'),
+        'constellation': ('constellation_id', 'constellation_name'),
+        'region': ('region_id', 'region_name'),
+    }
+
+    def _format_sparse(self, request, fields, killmail=None, killmails=None):
+        """Helper method for implementing `filter_sparse`
+
+        `request` should be a `dict`, as well as `killmail` (if provided).
+        `fields` should be a `set` of strings to include as keys in the output.
+
+        If `fields` contains fields that need to be looked up on a `Killmail`
+        instance and either `killmail` nor `killmails` are given, an exception
+        will be raised.
+        `killmails` is a dict, with the keys being the integer IDs of the
+        killmails, and the value being a `dict` of the killmail itself.
+        """
+        if not self._killmail_fields.isdisjoint(fields) and \
+                killmail == killmails is None:
+            raise ValueError(u"Either 'killmail' or 'killmails' must be given"
+                             u" if a killmail field is being returned.")
+        elif killmail is None and killmails is not None:
+            killmail = killmails[request['killmail_id']]
+        # separate the field names into actual fields on objects, and things to
+        # look up in another database/store.
+        simple_fields = set(fields)
+        active_combo_fields = {}
+        for combo_field, replacement_fields in six.iteritems(
+                self._combo_fields):
+            if combo_field in simple_fields:
+                active_combo_fields[combo_field] = replacement_fields
+                simple_fields.remove(combo_field)
+        # Construct the sparse request dictionary
+        sparse_request = {}
+        # A helper function to look up values from the appropriate object
+        def lookup_field(field_name):
+            # the timestamp fields are named the same on their objects
+            if field_name.endswith('_timestamp'):
+                real_field_name = 'timestamp'
+            else:
+                real_field_name = field_name
+            if field_name in self._request_fields:
+                return request[real_field_name]
+            elif field_name in self._killmail_fields:
+                return killmail[real_field_name]
+        # First add the simple (aka non-combo) fields
+        for field in simple_fields:
+            sparse_request[field] = lookup_field(field)
+        # Now add the combo fields
+        for combo_field, replacement_fields in six.iteritems(
+                active_combo_fields):
+            id_field, name_field = replacement_fields
+            id_ = lookup_field(id_field)
+            # Some names are able to be looked up through this app's database,
+            # while most of the others need to be looked up from CCP.
+            if id_field in ('division_id', 'pilot_id'):
+                # Being a little clever here, lookup the appropriate get_
+                # method on this store for the thing we're looking up.
+                get_object = getattr(self, 'get_' + combo_field)
+                name = get_object(id_)['name']
+            else:
+                # TODO: CCP Lookup
+                pass
+            sparse_request[combo_field] = {
+                'id': id_,
+                'name': name,
+            }
+        return sparse_request
 
     def filter_sparse(self, filters, fields):
-        pass
+        full_requests = self.filter_requests(filters)
+        format_kwargs = {'fields': fields}
+        if not self._killmail_fields.isdisjoint(fields):
+            killmail_ids = {r['killmail_id'] for r in full_requests}
+            full_killmails = self.get_killmails(killmail_ids=killmail_ids)
+            format_kwargs['killmails'] = {km['id']: km for km in full_killmails}
+        for request in full_requests:
+            yield self._format_sparse(request, **format_kwargs)
 
     ### Misc ###
 
     def get_pilot(self, pilot_id):
-        pass
+        raise NotImplementedError
 
-    def get_notes(self, **kwargs):
-        pass
-
+    def get_notes(self, subject_id):
+        raise NotImplementedError
---
+++ tests_python/rearch/test_storage_base.py
@@ -0,0 +1,164 @@
+import inspect
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+import pytest
+from evesrp import storage
+from evesrp import search_filter as sfilter
+
+
+not_implemented = [
+    'get_division', 'get_divisions', 'add_division', 'save_division',
+    'get_permission', 'get_permissions', 'add_permission', 'remove_permission',
+    'get_user', 'get_users', 'get_groups', 'associate_user_group',
+    'disassociate_user_group',
+    'get_killmail', 'get_killmails',
+    'get_request', 'get_requests', 'add_request', 'save_request',
+    'get_action', 'get_actions', 'add_action',
+    'get_modifier', 'get_modifiers', 'add_modifier', 'save_modifier',
+    'filter_requests',
+    'get_pilot', 'get_notes',
+]
+
+
+@pytest.mark.parametrize('method_name', not_implemented)
+def test_not_implemented(method_name):
+    store = storage.BaseStore()
+    method = getattr(store, method_name)
+    # subtract one for self
+    arg_count = len(inspect.getargspec(method).args) - 1
+    with pytest.raises(NotImplementedError):
+        # This silliness with the range splat is just to fill in required
+        # arguments.
+        method(*range(arg_count))
+
+
+@pytest.fixture
+def mock_filter_store():
+    store = storage.BaseStore()
+    store.filter_requests = mock.Mock()
+    store.get_pilot = mock.Mock()
+    store.get_division = mock.Mock()
+    store.get_killmails = mock.Mock()
+    return store
+
+
+@pytest.fixture(params=(
+    ('killmail_id', 'submit_timestamp'),
+    ('killmail_id', 'kill_timestamp'),
+    ('killmail_id', 'division'),
+    ('killmail_id', 'pilot'),
+    pytest.mark.xfail(('killmail_id', 'type')),
+), ids=(
+    'simple_request',
+    'simple_request_simple_killmail',
+    'simple_request_combo_request_app',
+    'simple_request_combo_killmail_app',
+    'simple_request_combo_killmail_cpp',
+))
+def fields(request):
+    return request.param
+
+
+@pytest.mark.parametrize('single_killmail', (True, False),
+                         ids=('single_killmail', 'multiple_killmails'))
+def test_format_sparse(mock_filter_store, fields, single_killmail):
+    format_kwargs = {}
+    if 'kill_timestamp' in fields or \
+            'pilot' in fields or \
+            'type' in fields:
+        # We need to create a fake killmail
+        killmail = {
+            'id': 1234,
+            'pilot_id': 2468,
+            'type_id': 1357,
+            'timestamp': mock.sentinel.kill_timestamp,
+        }
+        if single_killmail:
+            format_kwargs['killmail'] = killmail
+        else:
+            format_kwargs['killmails'] = {
+                killmail['id']:killmail,
+            }
+        if 'pilot' in fields:
+            mock_filter_store.get_pilot.return_value = {
+                'name': mock.sentinel.pilot_name,
+            }
+    request = {
+        'killmail_id': 1234,
+        'timestamp': mock.sentinel.submit_timestamp,
+        'division_id': 8642,
+    }
+    if 'division' in fields:
+        mock_filter_store.get_division.return_value = {
+            'name': mock.sentinel.division_name,
+        }
+    sparse_request = mock_filter_store._format_sparse(
+        request, fields, **format_kwargs)
+    assert set(sparse_request.keys()) == set(fields)
+    assert sparse_request['killmail_id'] == 1234
+    if 'submit_timestamp' in fields:
+        assert sparse_request['submit_timestamp'] == \
+            mock.sentinel.submit_timestamp
+    if 'kill_timestamp' in fields:
+        assert sparse_request['kill_timestamp'] == \
+            mock.sentinel.kill_timestamp
+    if 'division' in fields:
+        assert sparse_request['division'] == {
+            'id': 8642,
+            'name': mock.sentinel.division_name,
+        }
+        mock_filter_store.get_division.assert_called_with(8642)
+    else:
+        assert not mock_filter_store.get_division.called
+    if 'pilot' in fields:
+        assert sparse_request['pilot'] == {
+            'id': 2468,
+            'name': mock.sentinel.pilot_name,
+        }
+        mock_filter_store.get_pilot.assert_called_with(2468)
+    else:
+        assert not mock_filter_store.get_pilot.called
+    if 'type' in fields:
+        assert sparse_request['type'] == {
+            'id': None,
+            'name': mock.sentinel
+        }
+
+
+def test_format_sparse_exception():
+    store = storage.BaseStore()
+    with pytest.raises(ValueError):
+        store._format_sparse(dict(), {'kill_timestamp',})
+
+
+def test_filter_sparse(mock_filter_store, fields):
+    mock_filter_store.filter_requests.return_value = [
+        {'killmail_id': 1,},
+        {'killmail_id': 2,},
+        {'killmail_id': 3,},
+    ]
+    mock_filter_store.get_killmails.return_value = [
+        {'id': 1,},
+        {'id': 2,},
+        {'id': 3,},
+    ]
+    mock_filter_store._format_sparse = mock.Mock()
+    mock_filter_store._format_sparse.return_value = mock.sentinel.sparse
+    sparse_requests = list(mock_filter_store.filter_sparse(
+        mock.sentinel.filters, fields))
+    mock_filter_store.filter_requests.assert_called_with(mock.sentinel.filters)
+    if 'kill_timestamp' in fields or \
+            'pilot' in fields or \
+            'type' in fields:
+        mock_filter_store.get_killmails.assert_called_with(
+            killmail_ids=set((1, 2, 3)))
+    else:
+        assert not mock_filter_store.get_killmails.called
+    assert sparse_requests == [
+        mock.sentinel.sparse,
+        mock.sentinel.sparse,
+        mock.sentinel.sparse,
+    ]
+
