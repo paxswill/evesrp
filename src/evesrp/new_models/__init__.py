e51fcf3ea67a2bba58824acd8b31d119ad19c36c
Will Ross <paxswill@paxswill.com>
2017-03-06 16:06:26 -0500

    Make imports from the new models a little easier

--- src/evesrp/new_models/__init__.py
+++ src/evesrp/new_models/__init__.py
@@ -1,4 +1,9 @@
+from .authentication import AuthenticatedUser, AuthenticatedGroup
 from .authorization import Entity, User, Group, Division, Permission, \
                            PermissionType, Note
 from .request import Character, Killmail, Request, Action, Modifier, \
                      ActionType, ModifierType
+
+# Alias the two auth modules to shorter names
+from . import authentication as authn
+from . import authorization as authz
--- tests_python/rearch/test_authorization.py
+++ tests_python/rearch/test_authorization.py
@@ -4,9 +4,10 @@ try:
 except ImportError:
     import mock
 import pytest
+
 from evesrp import storage
+from evesrp import new_models as models
 from evesrp.util import utc
-from evesrp.new_models import authorization as authz
 
 
 @pytest.fixture
@@ -15,7 +16,7 @@ def store():
 
 
 def test_entity_init():
-    entity = authz.Entity("An Entity", 12345)
+    entity = models.authz.Entity("An Entity", 12345)
     assert entity.name == "An Entity"
     assert entity.id_ == 12345
 
@@ -25,20 +26,20 @@ def test_entity_dict():
         'name': 'An Entity',
         'id': 12345,
     }
-    entity = authz.Entity.from_dict(user_dict)
+    entity = models.authz.Entity.from_dict(user_dict)
     assert entity.name == "An Entity"
     assert entity.id_ == 12345
 
 
 def test_entity_permissions(store):
-    PT = authz.PermissionType
+    PT = models.PermissionType
     entity_perms = [
-        authz.Permission(entity_id=1, division_id=1, type_=PT.submit),
-        authz.Permission(entity_id=1, division_id=1, type_=PT.pay),
-        authz.Permission(entity_id=1, division_id=3, type_=PT.submit),
+        models.Permission(entity_id=1, division_id=1, type_=PT.submit),
+        models.Permission(entity_id=1, division_id=1, type_=PT.pay),
+        models.Permission(entity_id=1, division_id=3, type_=PT.submit),
     ]
     store.get_permissions.return_value = entity_perms
-    entity = authz.Entity("An Entity", 1)
+    entity = models.authz.Entity("An Entity", 1)
     permission_tuples = {
         (PT.submit, 1),
         (PT.pay, 1),
@@ -49,7 +50,7 @@ def test_entity_permissions(store):
 
 @pytest.mark.parametrize('is_admin', [True, False])
 def test_user_init(is_admin):
-    user = authz.User('A User', 1, is_admin)
+    user = models.User('A User', 1, is_admin)
     assert user.name == 'A User'
     assert user.id_ == 1
     assert user.admin == is_admin
@@ -61,7 +62,7 @@ def test_user_dict():
         'id': 73,
         'admin': True,
     }
-    user = authz.User.from_dict(user_dict)
+    user = models.User.from_dict(user_dict)
     assert user.name == 'Another User'
     assert user.id_ == 73
     assert user.admin == True
@@ -69,30 +70,30 @@ def test_user_dict():
 
 def test_user_get_groups(store):
     groups = [
-        authz.Group("Group 1", 1),
-        authz.Group("Group 3", 3),
+        models.Group("Group 1", 1),
+        models.Group("Group 3", 3),
     ]
     store.get_groups.return_value = groups
-    user = authz.User("User 2", 2)
+    user = models.User("User 2", 2)
     assert user.get_groups(store) == groups
 
 
 def test_user_permissions(store):
-    PT = authz.PermissionType
+    PT = models.PermissionType
     # Doing a bit more involved mock here as we want to return different values
     # for store.get_permissions based on the value of entity_id passed in, as
     # well as mocking get_groups
     user1_perms = [
-        authz.Permission(entity_id=1, division_id=1, type_=PT.submit),
-        authz.Permission(entity_id=1, division_id=2, type_=PT.review),
+        models.Permission(entity_id=1, division_id=1, type_=PT.submit),
+        models.Permission(entity_id=1, division_id=2, type_=PT.review),
     ]
     group2_perms = [
-        authz.Permission(entity_id=2, division_id=2, type_=PT.review),
-        authz.Permission(entity_id=2, division_id=2, type_=PT.submit),
-        authz.Permission(entity_id=2, division_id=3, type_=PT.pay),
+        models.Permission(entity_id=2, division_id=2, type_=PT.review),
+        models.Permission(entity_id=2, division_id=2, type_=PT.submit),
+        models.Permission(entity_id=2, division_id=3, type_=PT.pay),
     ]
     store.get_groups.return_value = [
-        authz.Group("Group 2", 2),
+        models.Group("Group 2", 2),
     ]
     def get_permissions(entity_id):
         if entity_id == 9:
@@ -100,7 +101,7 @@ def test_user_permissions(store):
         if entity_id == 2:
             return group2_perms
     store.get_permissions.side_effect = get_permissions
-    user = authz.User("User 9", 9)
+    user = models.User("User 9", 9)
     union_permissions = {
         (PT.submit, 1),
         (PT.review, 2),
@@ -112,9 +113,9 @@ def test_user_permissions(store):
 
 
 def test_user_notes(store):
-    user = authz.User("Notable User", 1)
+    user = models.User("Notable User", 1)
     notes = [
-        authz.Note("A note.", 1, subject_id=1, submitter_id=2),
+        models.Note("A note.", 1, subject_id=1, submitter_id=2),
     ]
     store.get_notes.return_value = notes
     assert user.get_notes(store) == notes
@@ -122,13 +123,13 @@ def test_user_notes(store):
 
 @pytest.fixture
 def group():
-    return authz.Group("Group 1", 1)
+    return models.Group("Group 1", 1)
 
 
 def test_group_get_users(store, group):
     users = [
-        authz.User("User 2", 2),
-        authz.User("User 4", 4),
+        models.User("User 2", 2),
+        models.User("User 4", 4),
     ]
     store.get_users.return_value = users
     assert group.get_users(store) == users
@@ -148,7 +149,7 @@ def test_group_remove_user(store, group):
 
 @pytest.fixture
 def division():
-    return authz.Division("A Division", 98765)
+    return models.Division("A Division", 98765)
 
 
 def test_division_init(division):
@@ -161,14 +162,14 @@ def test_division_dict():
         'name': 'A Division',
         'id': 98765,
     }
-    division = authz.Division.from_dict(division_dict)
+    division = models.Division.from_dict(division_dict)
     assert division.name == "A Division"
     assert division.id_ == 98765
 
 
 def test_division_add_permission(store, division):
     permission = division.add_permission(store, entity_id=13,
-                                         type_=authz.PermissionType.submit)
+                                         type_=models.PermissionType.submit)
     assert permission is not None
     store.add_permission.assert_called_with(permission)
 
@@ -191,30 +192,30 @@ def test_division_get_permissions(store, division):
     permissions = division.get_permissions(store)
     assert permissions == mock_permissions[0]
     store.get_permissions.assert_called_with(division_id=division.id_)
-    permissions = division.get_permissions(store, authz.PermissionType.submit)
+    permissions = division.get_permissions(store, models.PermissionType.submit)
     assert permissions == mock_permissions[1]
     store.get_permissions.assert_called_with(division_id=division.id_,
                                              types=(
-                                                 authz.PermissionType.submit,
+                                                 models.PermissionType.submit,
                                              ))
 
 
 def test_permission_init_objects():
-    user = authz.User("A User", 1357)
-    division = authz.Division("A Division", 2468)
-    permission = authz.Permission(entity=user, division=division,
-                                  type_=authz.PermissionType.submit)
+    user = models.User("A User", 1357)
+    division = models.Division("A Division", 2468)
+    permission = models.Permission(entity=user, division=division,
+                                  type_=models.PermissionType.submit)
     assert permission.entity_id == user.id_
     assert permission.division_id == division.id_
-    assert permission.type_ == authz.PermissionType.submit
+    assert permission.type_ == models.PermissionType.submit
 
 
 def test_permission_init_object_ids():
-    permission = authz.Permission(entity_id=1357, division_id=2468,
-                                  type_=authz.PermissionType.submit)
+    permission = models.Permission(entity_id=1357, division_id=2468,
+                                  type_=models.PermissionType.submit)
     assert permission.entity_id == 1357
     assert permission.division_id == 2468
-    assert permission.type_ == authz.PermissionType.submit
+    assert permission.type_ == models.PermissionType.submit
 
 
 def test_permission_dict():
@@ -223,16 +224,16 @@ def test_permission_dict():
         'division_id': 200,
         'type': 'submit',
     }
-    permission = authz.Permission.from_dict(permission_dict)
-    assert permission.type_ == authz.PermissionType.submit
+    permission = models.Permission.from_dict(permission_dict)
+    assert permission.type_ == models.PermissionType.submit
     assert permission.entity_id == 100
     assert permission.division_id == 200
 
 
 def test_permission_tuples():
-    permission = authz.Permission(entity_id=1, division_id=2,
-                                  type_=authz.PermissionType.review)
-    assert permission.to_tuple() == (authz.PermissionType.review, 2)
+    permission = models.Permission(entity_id=1, division_id=2,
+                                  type_=models.PermissionType.review)
+    assert permission.to_tuple() == (models.PermissionType.review, 2)
 
 
 @pytest.mark.parametrize('timestamp', [
@@ -244,7 +245,7 @@ def test_note_init(timestamp):
     # If timestamp is None in the Note __init__, it uses utcnow.
     if timestamp is None:
         before = dt.datetime.utcnow()
-    note = authz.Note(contents, 1, timestamp, subject_id=2,
+    note = models.Note(contents, 1, timestamp, subject_id=2,
                       submitter_id=3)
     if timestamp is None:
         after = dt.datetime.utcnow()
@@ -268,7 +269,7 @@ def test_note_dict():
         "submitter_id": 22,
         "subject_id": 33,
     }
-    note = authz.Note.from_dict(note_dict)
+    note = models.Note.from_dict(note_dict)
     assert note.id_ == 11
     assert note.submitter_id == 22
     assert note.subject_id == 33
