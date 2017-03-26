b10a63b472be52b7822d8d74c626f07e81005b10
Will Ross <paxswill@paxswill.com>
2017-03-25 22:20:15 -0400

    Allow setting DivisionAdmin.user after creation

--- src/evesrp/users/authorization.py
+++ src/evesrp/users/authorization.py
@@ -29,19 +29,29 @@ class DivisionAdmin(object):
         self.store = store
         if isinstance(user, six.integer_types):
             user = self.store.get_user(user_id=user)
-        self.user = user
         if isinstance(division, six.integer_types):
             division = self.store.get_division(division_id=division)
+        # self.division _MUST_ be set before setting self.user
         self.division = division
-        if not self.user.admin:
+        self.user = user
+
+    @property
+    def user(self):
+        return self._user
+
+    @user.setter
+    def user(self, new_user):
+        if not new_user.admin:
             permissions = self.store.get_permissions(
+                entity_id=new_user.id_,
                 division_id=self.division.id_,
                 type_=models.PermissionType.admin)
             if not permissions:
                 error_message = (u"User '{}' has insufficient permissions to "
                                  u"administer divisions.")
-                error_message = error_message.format(self.user.id_)
+                error_message = error_message.format(new_user.id_)
                 raise errors.AdminPermissionError(error_message)
+        self._user = new_user
 
     def list_permissions(self, type_):
         permissions = self.store.get_permissions(division_id=self.division.id_,
