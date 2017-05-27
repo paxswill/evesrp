985ac44d21388e7e37d2f96d578ba5314d26f73c
Will Ross <paxswill@paxswill.com>
2017-05-27 17:36:05 -0400

    Add test for Query.users field

--- tests_python/rearch/graphql/resolver/test_base_query.py
+++ tests_python/rearch/graphql/resolver/test_base_query.py
@@ -177,3 +177,30 @@ class TestNode(object):
         expected_user_ids = {to_global_id('User', uid) for uid in (2, 9)}
         result_user_ids = {u['id'] for u in results['data']['node']['users']}
         assert expected_user_ids == result_user_ids
+
+
+@pytest.mark.parametrize(
+    'group_id,expected_user_ids',
+    (
+        (None, (9, 2, 7)),
+        (to_global_id('Group', 3000), (9, )),
+        (to_global_id('Group', 5000), (2, 9)),
+    ),
+    ids=('all_users', 'one_user', 'multiple_users')
+)
+def test_users(graphql_client, group_id, expected_user_ids):
+    query = '''
+    query getUsers($groupID: ID) {
+        users(groupId: $groupID) {
+            id
+        }
+    }
+    '''
+    result = graphql_client.execute(query,
+                                    variable_values={'groupID': group_id})
+    assert 'data' in result
+    ids = {user['id'] for user in result['data']['users']}
+    expected_relay_ids = {to_global_id('User', uid) for uid in
+                          expected_user_ids}
+    assert ids == expected_relay_ids
+
