66af9d6d5e6fc11fb1174804e387a041c6f1c0ef
Will Ross <paxswill@paxswill.com>
2017-10-24 13:11:25 -0400

    v0.12.11: Fixing jsonify arguments
    Thanks to Krylik for the patch

--- src/evesrp/__init__.py
+++ src/evesrp/__init__.py
@@ -62,7 +62,7 @@ _patch_httplib()
 from .util import DB_STATS, AcceptRequest, WeakCiphersAdapter
 
 
-__version__ = u'0.12.11.dev'
+__version__ = u'0.12.11'
 
 
 requests_session = requests.Session()
