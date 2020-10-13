5e6375bf7c6fe8d7e94a7c76c4223c4073647414
Will Ross <paxswill@paxswill.com>
2020-10-13 17:25:19 -0400

    Release v0.12.12: Security Update

--- src/evesrp/__init__.py
+++ src/evesrp/__init__.py
@@ -62,7 +62,7 @@ _patch_httplib()
 from .util import DB_STATS, AcceptRequest, WeakCiphersAdapter
 
 
-__version__ = u'0.12.12.dev'
+__version__ = u'0.12.12'
 
 
 requests_session = requests.Session()
