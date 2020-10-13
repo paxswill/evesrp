01e3658007b19e25f6fe0622389ed4f23b7f95a2
Will Ross <paxswill@paxswill.com>
2020-10-13 17:26:14 -0400

    Start 0.12.13 development
    Not that I'm developing this anymore...

--- src/evesrp/__init__.py
+++ src/evesrp/__init__.py
@@ -62,7 +62,7 @@ _patch_httplib()
 from .util import DB_STATS, AcceptRequest, WeakCiphersAdapter
 
 
-__version__ = u'0.12.12'
+__version__ = u'0.12.13.dev'
 
 
 requests_session = requests.Session()
